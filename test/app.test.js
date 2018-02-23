8cdbcc435d2511edaa9ec2610d852458c72b7b3d
Maddy Dannenberg <mmdberg@gmail.com>
2018-02-22 17:40:15 -0700

    Add app testing for starting states, city state change from userinput and rendering search

--- test/app.test.js
+++ test/app.test.js
@@ -13,6 +13,29 @@ describe('App', () => {
     expect(wrapper).toBeDefined();
   });
 
+  it('should start with state of no city and no currentWeather', () => {
+    expect(wrapper.state().city).toEqual('')
+    expect(wrapper.state().currentWeather).toEqual([])
+  })
+
+  it('should get city info from search bar', () => {
+    wrapper.instance().getWeather = jest.fn()
+
+
+    expect(wrapper.state().city).toEqual('');
+    wrapper.instance().getCity('Miami, FL');
+    expect(wrapper.state().city).toEqual('Miami, FL');
+
+      //should pull from local storage
+  })
+
+  it('should render search bar, current weather, seven hour and ten day', () => {
+    expect(wrapper.find('Search').length).toEqual(1);
+    // expect(wrapper.find('CurrentWeather').length).toEqual(1);
+    // expect(wrapper.find('SevenHourForecast').length).toEqual(1);
+    // expect(wrapper.find('TenDayForecast').length).toEqual(1);
+
+  })
   
 
 })
